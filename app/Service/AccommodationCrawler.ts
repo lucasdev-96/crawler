9f8eeb92f5d72ac8de69605bd734dd4a2c6e9d1c
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-04-05 22:02:14 -0300

    refactor: :recycle: add interface

--- app/Service/AccommodationCrawler.ts
+++ app/Service/AccommodationCrawler.ts
@@ -3,24 +3,28 @@ import { DateTime } from 'luxon'
 import { Page } from 'puppeteer'
 
 import CrawlerService from './Crawler'
+import IAccommodationCrawlerInterface from 'App/interfaces/AccommodationCrawlerInterface'
 
 @inject()
 export default class AccommodationCrawler {
   constructor(private crawler: CrawlerService) {}
-  public async start(checkout: DateTime, checkin: DateTime): Promise<string | any> {
+  public async start(
+    checkout: DateTime,
+    checkin: DateTime
+  ): Promise<string | Array<IAccommodationCrawlerInterface>> {
     const page = await this.crawler.start(this.getUrl(checkout, checkin))
     const result = await this.getRooms(page)
     page.close()
     return result
   }
 
-  private getUrl(checkout: DateTime, checkin: DateTime) {
+  private getUrl(checkout: DateTime, checkin: DateTime): string {
     const formatCheckin = DateTime.fromISO(checkin.toString()).toObject()
     const formatCheckout = DateTime.fromISO(checkout.toString()).toObject()
     return `https://pratagy.letsbook.com.br/D/Reserva?checkin=${formatCheckin.day}%2F${formatCheckin.month}%2F${formatCheckin.year}&checkout=${formatCheckout.day}%2F${formatCheckout.month}%2F${formatCheckout.year}&cidade=&hotel=12&adultos=2&criancas=&destino=Pratagy+Beach+Resort+All+Inclusive&promocode=&tarifa=`
   }
 
-  private async getRooms(page: Page): Promise<string | any> {
+  private async getRooms(page: Page): Promise<string | Array<IAccommodationCrawlerInterface>> {
     const hotelAvaible = await this.verifyAvaibleHotel(page)
     if (!hotelAvaible) {
       await page.waitForSelector('div > div.left-col > ul > div.slick-list.draggable')
@@ -40,12 +44,11 @@ export default class AccommodationCrawler {
     }
     return hotelAvaible
   }
-  private async verifyAvaibleHotel(page: Page): Promise<string | boolean> {
+  private async verifyAvaibleHotel(page: Page): Promise<string | undefined> {
     await page.waitForSelector('#hotelSelecionadoContent')
     const fatherNoHotel = await page.$('#hotel-selecionado-indisponivel')
     if (fatherNoHotel) {
       return page.$eval('#hotel-selecionado-indisponivel > div:nth-child(2)', (e) => e.textContent)
     }
-    return false
   }
 }
